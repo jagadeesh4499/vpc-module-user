4cd337ac4310b5b868ccda5d6697fce9f516369f
jagadeesh <jagadeeshakula123@gmail.com>
2024-09-30 00:16:24 +0530

    vpc-module-user

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source                = "../vpc-module-developer"
+  source = "../vpc-module-developer"
   #Using Terraform module from GitHub
   #source = "git::git-URL?ref=branch-name"
   vpc_cidr              = var.vpc_cidr
